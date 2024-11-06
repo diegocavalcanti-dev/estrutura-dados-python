ba1747bca3f3467ec2ab3c28b6d2d7015a62e497
Diego Cavalcanti <cavalcantidiego@hotmail.com>
2024-11-06 07:53:28 -0300

    update

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-# Estruturas de Dados
+# Estruturas de Dados ------------------------------
 
 # Listas
 
@@ -49,3 +49,67 @@ rede = {'nome': 'uai-fi', 'senha': 'r3d3'}
 wifi_disponiveis.append(rede)
 
 print(wifi_disponiveis)
+
+
+# Fluxo condicional e Repetição ------------------------------
+
+# Estrutura condicional if / else / elif
+
+# if <booleano / comparação lógica> == True:
+#   <execute este código>
+# else:
+#   <senão execute este código>
+
+
+# if False:
+# print("Verdadeiro")
+#     else:
+# print("Falso")
+
+
+codigo_de_seguranca = '291'
+codigo_de_seguranca_cadastro = '010'
+
+pode_efetuar_pagamento = codigo_de_seguranca == codigo_de_seguranca_cadastro
+print(pode_efetuar_pagamento)
+
+if pode_efetuar_pagamento:
+  print("Pagamento efetuado")
+else:
+  print("Erro: Código de segurança inválido")
+  
+if codigo_de_seguranca == codigo_de_seguranca_cadastro:
+  print("Pagamento efetuado")
+else:
+  print("Erro: Código de segurança inválido")
+  
+  
+# 1.2. if / elif / else
+# Podemos também avaliar múltipla condições.
+
+# if <1º booleano / 1ª comparação lógica> == True:
+#   <execute este código se a primeira condição for verdade>
+# elif <2º booleano / 2ª comparação lógica> == True:
+#   <execute este código se a segunda condição for verdade>
+# else:
+#   <senão execute este código>
+
+
+codigo_de_seguranca = '802'
+codigo_de_seguranca_cadastro = '852'
+
+senha = '7703'
+senha_cadastro = '7783'
+
+
+if (codigo_de_seguranca == codigo_de_seguranca_cadastro) & (senha == senha_cadastro):
+  print("Pagamento efetuado")
+
+elif (codigo_de_seguranca != codigo_de_seguranca_cadastro) & (senha == senha_cadastro):
+  print("Erro: Código de segurança inválido")
+
+elif (codigo_de_seguranca == codigo_de_seguranca_cadastro) & (senha != senha_cadastro):
+  print("Erro: Senha inválida inválida")
+
+else:
+  print("Erro: Código de segurança e senha inválidos")
