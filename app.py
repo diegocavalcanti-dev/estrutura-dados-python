9c97a26f00d84b292ba72ef776a6126ce541405c
Diego Cavalcanti <cavalcantidiego@hotmail.com>
2024-11-06 14:57:51 -0300

    update

--- app.py
+++ app.py
@@ -113,3 +113,103 @@ elif (codigo_de_seguranca == codigo_de_seguranca_cadastro) & (senha != senha_cad
 
 else:
   print("Erro: Código de segurança e senha inválidos")
+  
+  
+  
+  
+# Estrutura condicional try /except / finally
+# 2.1. Exceção
+# Exceções são erros que podem acontecer durante a execução do nosso código.
+
+# Exemplo: Erro de operações numéricas impossíveis
+preco = 132.85
+pessoas = 0
+
+valor_por_pessoa = preco / pessoas
+
+
+# Exemplo: Erro por combinações de tipos diferentes
+nome = 'Andre Perez'
+idade = True
+
+apresentacao = 'Fala pessoal, meu nome é ' + nome + ' e eu tenho ' + idade + ' anos'
+
+# Exemplo: Erro de indexação de estrutura de dados
+anos = [2019, 2020, 2021]
+ano_atual = anos[3]
+
+cursos = {
+    'python': {
+        'nome': 'Python para Análise de Dados', 'duracao': 2.5
+    }, 
+    'sql': {
+        'nome': 'SQL para Análise de Dados', 'duracao': 2
+    }
+}
+curso_atual = cursos['analista']
+
+# 2.2. try / except
+# Estrutura para tratar exceções:
+
+preco = 132.85
+pessoas = 2
+
+try:
+  valor_por_pessoa = preco / pessoas
+  print(valor_por_pessoa)
+except ZeroDivisionError:
+  print('Número de pessoas inválido. Espera-se um valor maior que 0 e obteve-se um valor igual a ' + str(pessoas))
+  
+#################  
+anos = [2019, 2020, 2021]
+
+try:
+  ano_atual = anos[3]
+  print(ano_atual)
+except Exception:
+  print('Lista de anos é menor que o valor escolhido. Espera-se um valor entre 0 e ' + str(len(anos) - 1))
+  
+anos = [2019, 2020, 2021]
+
+try:
+  ano_atual = anos[3]
+  print(ano_atual)
+except Exception as exc:
+  print('Descrição da exceção: ' + str(exc))
+  print('Tipo da exceção: ' + str(type(exc)))
+  print('Lista de anos é menor que o valor escolhido. Espera-se um valor entre 0 e ' + str(len(anos) - 1))
+  
+# 2.3. try / except / finally
+nome = 'Andre Perez'
+idade = 19
+
+try:
+  apresentacao = 'Fala pessoal, meu nome é ' + nome + ' e eu tenho ' + idade + ' anos'
+  print(apresentacao)
+except TypeError:
+  idade = str(idade)
+finally:
+  print('Segunda chance')
+  apresentacao = 'Fala pessoal, meu nome é ' + nome + ' e eu tenho ' + idade + ' anos'
+  print(apresentacao)
+  
+
+
+# Estrutura repetição for / in
+# 3.1. for / in
+# for variavel_temporaria in coleção:
+#   <execute este código>
+
+# 3.2. for / in / range
+# Estrutura que permite a execução repetida de um bloco de código n vezes.
+
+for valor in range(6):
+  print(valor)
+  
+soma = 0
+
+for valor in range(0, 100000):
+  soma = soma + valor
+  # print(soma)
+
+print(soma)
